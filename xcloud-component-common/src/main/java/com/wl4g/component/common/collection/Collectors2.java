0a0dd8f17050fb4863c671983f2b3baf21beac9d
wl4g <983708408@qq.com>
2022-01-07 16:16:52 +0800

    feat: add Collector2.toLinkedHashMap

--- xcloud-component-common/src/main/java/com/wl4g/component/common/collection/Collectors2.java
+++ xcloud-component-common/src/main/java/com/wl4g/component/common/collection/Collectors2.java
@@ -15,8 +15,14 @@
  */
 package com.wl4g.component.common.collection;
 
+import static java.util.stream.Collectors.toMap;
+
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -33,28 +39,82 @@ import java.util.stream.Collector.Characteristics;
  */
 public abstract class Collectors2 {
 
-	/**
-	 * Returns a {@code Collector} that accumulates the input elements into a
-	 * new {@code Set}. There are no guarantees on the type, mutability,
-	 * serializability, or thread-safety of the {@code Set} returned; if more
-	 * control over the returned {@code Set} is required, use
-	 * {@link Collectors#toCollection(Supplier)}.
-	 *
-	 * <p>
-	 * This is an {@link Collector.Characteristics#UNORDERED unordered}
-	 * Collector.
-	 *
-	 * @param <T>
-	 *            the type of the input elements
-	 * @return a {@code Collector} which collects all the input elements into a
-	 *         {@code Set}
-	 */
-	@SuppressWarnings("unchecked")
-	public static <T> Collector<T, ?, Set<T>> toLinkedHashSet() {
-		return Collector.of(LinkedHashSet::new, Set::add, (s, rs) -> {
-			s.add((T) rs);
-			return s;
-		}, Characteristics.IDENTITY_FINISH);
-	}
+    /**
+     * Returns a {@code Collector} that accumulates the input elements into a
+     * new {@code Set}. There are no guarantees on the type, mutability,
+     * serializability, or thread-safety of the {@code Set} returned; if more
+     * control over the returned {@code Set} is required, use
+     * {@link Collectors#toCollection(Supplier)}.
+     *
+     * <p>
+     * This is an {@link Collector.Characteristics#UNORDERED unordered}
+     * Collector.
+     *
+     * @param <T>
+     *            the type of the input elements
+     * @return a {@code Collector} which collects all the input elements into a
+     *         {@code Set}
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> Collector<T, ?, Set<T>> toLinkedHashSet() {
+        return Collector.of(LinkedHashSet::new, Set::add, (s, rs) -> {
+            s.add((T) rs);
+            return s;
+        }, Characteristics.IDENTITY_FINISH);
+    }
+
+    /**
+     * Returns a {@code Collector} that accumulates elements into a {@code Map}
+     * whose keys and values are the result of applying the provided mapping
+     * functions to the input elements.
+     *
+     * <p>
+     * If the mapped keys contains duplicates (according to
+     * {@link Object#equals(Object)}), the value mapping function is applied to
+     * each equal element, and the results are merged using the provided merging
+     * function. The {@code Map} is created by a provided supplier function.
+     *
+     * @implNote The returned {@code Collector} is not concurrent. For parallel
+     *           stream pipelines, the {@code combiner} function operates by
+     *           merging the keys from one map into another, which can be an
+     *           expensive operation. If it is not required that results are
+     *           merged into the {@code Map} in encounter order, using
+     *           {@link #toConcurrentMap(Function, Function, BinaryOperator, Supplier)}
+     *           may offer better parallel performance.
+     *
+     * @param <T>
+     *            the type of the input elements
+     * @param <K>
+     *            the output type of the key mapping function
+     * @param <U>
+     *            the output type of the value mapping function
+     * @param <M>
+     *            the type of the resulting {@code Map}
+     * @param keyMapper
+     *            a mapping function to produce keys
+     * @param valueMapper
+     *            a mapping function to produce values
+     * @param mergeFunction
+     *            a merge function, used to resolve collisions between values
+     *            associated with the same key, as supplied to
+     *            {@link Map#merge(Object, Object, BiFunction)}
+     * @param mapSupplier
+     *            a function which returns a new, empty {@code Map} into which
+     *            the results will be inserted
+     * @return a {@code Collector} which collects elements into a {@code Map}
+     *         whose keys are the result of applying a key mapping function to
+     *         the input elements, and whose values are the result of applying a
+     *         value mapping function to all input elements equal to the key and
+     *         combining them using the merge function
+     *
+     * @see #toMap(Function, Function)
+     * @see #toMap(Function, Function, BinaryOperator)
+     * @see #toConcurrentMap(Function, Function, BinaryOperator, Supplier)
+     */
+    @SuppressWarnings("unchecked")
+    public static <T, K, U, M extends Map<K, U>> Collector<T, ?, M> toLinkedHashMap(Function<? super T, ? extends K> keyMapper,
+            Function<? super T, ? extends U> valueMapper) {
+        return (Collector<T, ?, M>) toMap(keyMapper, valueMapper, (oldValue, newValue) -> oldValue, LinkedHashMap::new);
+    }
 
 }
---
+++ xcloud-component-common/src/test/java/com/wl4g/component/common/collection/Collector2Tests.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017 ~ 2025 the original author or authors. <wanglsir@gmail.com, 983708408@qq.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wl4g.component.common.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+/**
+ * {@link Collector2Tests}
+ * 
+ * @author Wangl.sir &lt;wanglsir@gmail.com, 983708408@qq.com&gt;
+ * @version 2022-01-07 v1.0.0
+ * @since v1.0.0
+ */
+public class Collector2Tests {
+
+    @Test
+    public void toLinkedHashMapCase1() {
+        List<String> list = new ArrayList<>();
+        list.add("jack1");
+        list.add("tom2");
+        list.add("mary3");
+        Map<String, Integer> result = list.stream().collect(Collectors2.toLinkedHashMap(e -> e, e -> 1));
+        System.out.println(result.getClass());
+        System.out.println(result);
+    }
+
+}
