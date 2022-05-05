e70df18dd039b04eee6d254a2e3487dec316276a
wl4g <983708408@qq.com>
2022-05-05 17:15:15 +0800

    feat: add CIDRTests

---
+++ dopaas-infra-common/src/test/java/com/wl4g/infra/common/net/CIDRTests.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2017 ~ 2025 the original author or authors. <wanglsir@gmail.com, 983708408@qq.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wl4g.infra.common.net;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+/**
+ * {@link CIDRTests}
+ * 
+ * @author Wangl.sir &lt;wanglsir@gmail.com, 983708408@qq.com&gt;
+ * @version 2022-05-05 v3.0.0
+ * @since v3.0.0
+ */
+public class CIDRTests {
+
+    @Test
+    public void testIpv4Parse() throws Exception {
+        Assertions.assertTrue(CIDR.newCIDR("192.168.3.0/24").getMask() == 24);
+    }
+
+}
