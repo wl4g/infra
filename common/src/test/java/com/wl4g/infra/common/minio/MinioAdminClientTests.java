8720e2adf6f63b69ee65423d31ef3b1097e10cca
James Wong <wanglsir@gmail.com>
2022-08-31 17:19:25 +0800

    fix: minio client tests

--- common/src/test/java/com/wl4g/infra/common/minio/MinioAdminClientTests.java
+++ common/src/test/java/com/wl4g/infra/common/minio/MinioAdminClientTests.java
@@ -16,6 +16,7 @@
 package com.wl4g.infra.common.minio;
 
 import static com.wl4g.infra.common.serialize.JacksonUtils.toJSONString;
+import static java.util.Collections.emptyList;
 
 import java.io.IOException;
 import java.security.InvalidKeyException;
@@ -24,6 +25,8 @@ import java.util.Map;
 
 import org.bouncycastle.crypto.InvalidCipherTextException;
 import org.junit.Test;
+
+import com.wl4g.infra.common.minio.UserInfo.Status;
 
 /**
  * {@link MinioAdminClientTests}
@@ -34,6 +37,11 @@ import org.junit.Test;
  */
 public class MinioAdminClientTests {
 
+    static String TEST_ADD_ACCESSKEY1 = "test_tenant1001_user1";
+    static String TEST_ADD_SECRETKEY1 = "12345678";
+    static String TEST_ADD_POLICY_NAME1 = "test_tenant1001_policy1";
+    static String TEST_ADD_POLICY_JSON1 = "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Action\":[\"s3:PutObject\",\"s3:GetBucketLocation\",\"s3:GetObject\"],\"Resource\":[\"arn:aws:s3:::tenant1001/*\"]}]}";
+
     @Test
     public void testGetUsers() throws InvalidKeyException, NoSuchAlgorithmException, InvalidCipherTextException, IOException {
         MinioAdminClient adminCilent = MinioAdminClient.builder()
@@ -42,6 +50,24 @@ public class MinioAdminClientTests {
                 .build();
         Map<String, UserInfo> users = adminCilent.listUsers();
         System.out.println(toJSONString(users));
+    }
+
+    @Test
+    public void testCreatePolicy() throws InvalidKeyException, NoSuchAlgorithmException, InvalidCipherTextException, IOException {
+        MinioAdminClient adminCilent = MinioAdminClient.builder()
+                .endpoint("localhost", 9000, false)
+                .credentials("minioadmin", "minioadmin")
+                .build();
+        adminCilent.addCannedPolicy(TEST_ADD_POLICY_NAME1, TEST_ADD_POLICY_JSON1);
+    }
+
+    @Test
+    public void testCreateUser() throws InvalidKeyException, NoSuchAlgorithmException, InvalidCipherTextException, IOException {
+        MinioAdminClient adminCilent = MinioAdminClient.builder()
+                .endpoint("localhost", 9000, false)
+                .credentials("minioadmin", "minioadmin")
+                .build();
+        adminCilent.addUser(TEST_ADD_ACCESSKEY1, Status.ENABLED, TEST_ADD_SECRETKEY1, TEST_ADD_POLICY_NAME1, emptyList());
     }
 
 }
--- common/src/test/java/com/wl4g/infra/common/minio/MinioClientTests.java
+++ common/src/test/java/com/wl4g/infra/common/minio/MinioClientTests.java
@@ -15,10 +15,16 @@
  */
 package com.wl4g.infra.common.minio;
 
+import java.security.NoSuchAlgorithmException;
+
 import org.junit.Test;
 
 import io.minio.BucketExistsArgs;
 import io.minio.MinioClient;
+import io.minio.credentials.AssumeRoleProvider;
+import io.minio.credentials.Credentials;
+import io.minio.http.HttpUtils;
+import okhttp3.OkHttpClient;
 
 /**
  * {@link MinioClientTests}
@@ -28,6 +34,8 @@ import io.minio.MinioClient;
  * @since v3.0.0
  */
 public class MinioClientTests {
+
+    static OkHttpClient defaultHttpClient = HttpUtils.newDefaultHttpClient(15_000, 15_000, 15_000);
 
     @Test
     public void testBucketExists() throws Exception {
@@ -39,4 +47,56 @@ public class MinioClientTests {
         System.out.println(exists);
     }
 
+    /**
+     * <pre>
+     * 前置条件:
+     * 1. 在 MinIO 控制台新建存储桶 test_tenant1001
+     * 2. 在 MinIO 控制台新建策略 test_tenant1001_policy1, 其策略值例如(对桶 test_tenant1001/ 下的所有对象授权了3种操作权限):
+     *    {
+     *        "Version": "2012-10-17", // 版本号固定值
+     *        "Statement": [
+     *            {
+     *                "Effect": "Allow",
+     *                "Action": [
+     *                    "s3:GetBucketLocation",
+     *                    "s3:GetObject",
+     *                    "s3:PutObject"
+     *                ],
+     *                "Resource": [
+     *                    "arn:aws:s3:::test_tenant1001/*"
+     *                ]
+     *            }
+     *        ]
+     *    }
+     * 3. 在 MinIO 控制台新建用户 accessKey=test_tenant1001_user1, secretKey=12345678
+     * 4. 在 MinIO 控制台给用户 test_tenant1001_user1 分配策略 test_tenant1001_policy1
+     * </pre>
+     *
+     * 也可以使用如{@link com.wl4g.infra.common.minio.MinioAdminClientTests#testCreatePolicy()}的SDK实现自动创建.
+     * 
+     * @see https://github.com/minio/minio/delete/8.4.3/docs/sts/client-grants.md
+     */
+    @Test
+    public void testGetSTSWithAssumeRoleWithClientGrants() throws NoSuchAlgorithmException {
+        String stsEndpoint = "http://localhost:9000";
+        String accessKey = "test_tenant1001_user1";
+        String secretKey = "12345678";
+        int durationSeconds = 5 * 60;
+        // 本次操作需要申请的策略(最多可对桶 tenant1001/* 申请3种权限)
+        String applyPolicy = "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Action\":[\"s3:GetObject\"],\"Resource\":[\"arn:aws:s3:::test_tenant1001/*\"]}]}";
+        String region = "us-east-1";
+        String roleSessionName = "anySession"; // 任意
+        // 本次申请资源标识
+        String roleArn = "arn:aws:s3:::test_tenant1001/*";
+        String externalId = "myapp";
+
+        AssumeRoleProvider provider = new AssumeRoleProvider(stsEndpoint, accessKey, secretKey, durationSeconds, applyPolicy,
+                region, roleArn, roleSessionName, externalId, defaultHttpClient);
+
+        Credentials credentials = provider.fetch();
+        System.out.println("accessKey:" + credentials.accessKey());
+        System.out.println("secretKey:" + credentials.secretKey());
+        System.out.println("sessionToken:" + credentials.sessionToken());
+        System.out.println("expired:" + credentials.isExpired());
+    }
 }
